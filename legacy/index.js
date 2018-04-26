74bf739e6890a597cac17de29515f503b2476739
Aaron Greenspan <afg419@gmail.com>
2018-04-26 17:13:14 -0600

    includes js files

--- legacy/index.js
+++ legacy/index.js
@@ -8,4 +8,10 @@ var Type_Category;
     Type_Category[Type_Category["list"] = 3] = "list";
     Type_Category[Type_Category["trellis"] = 4] = "trellis";
 })(Type_Category = exports.Type_Category || (exports.Type_Category = {}));
-//# sourceMappingURL=legacy.js.map
+var Operation_Type;
+(function (Operation_Type) {
+    Operation_Type[Operation_Type["add"] = 0] = "add";
+    Operation_Type[Operation_Type["clear"] = 1] = "clear";
+    Operation_Type[Operation_Type["remove"] = 2] = "remove";
+})(Operation_Type = exports.Operation_Type || (exports.Operation_Type = {}));
+//# sourceMappingURL=index.js.map
