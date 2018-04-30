0daf6e292c5f69045925311d3bda7998183b5f67
Aaron Greenspan <afg419@gmail.com>
2018-04-30 14:49:48 -0600

    adds .d.ts

--- src/index.d.ts
+++ src/index.d.ts
@@ -8,7 +8,7 @@ export declare type CreateSeed<T, DbPopulatedFields extends keyof T, ForeignKeyA
 export declare type UpdateSeed<T, ForeignKeyArrayFields extends keyof T> = {
     [k in ForeignKeyArrayFields]?: Operation[];
 } & Partial<Omit<T, ForeignKeyArrayFields>>;
-export interface StrictCollection<T, DbPopulatedFields extends keyof T, ForeignKeyArrayFields extends keyof T> {
+export interface StrictCollection<T, DbPopulatedFields extends keyof T = NoKeys, ForeignKeyArrayFields extends keyof T = NoKeys> {
     getTrellis(): CollectionTrellis<T>;
     getTableClient(): void;
     create(seed: CreateSeed<T, DbPopulatedFields, ForeignKeyArrayFields>): Promise<T>;
@@ -20,3 +20,4 @@ export interface StrictCollection<T, DbPopulatedFields extends keyof T, ForeignK
     first(options?: Partial<T>): QueryBuilder<T, T | undefined>;
     get(identity: any): any;
 }
+export declare type NoKeys = never;
